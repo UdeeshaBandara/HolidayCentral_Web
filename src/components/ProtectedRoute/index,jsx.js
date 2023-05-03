58dfe32bec9b299a2186d9027fb29d361fb80bf5
udeeshabandara <udeeshabandara@gmail.com>
2023-05-03 22:31:07 +0530

    feat:applying protected route

---
+++ src/components/ProtectedRoute/index,jsx.js
@@ -0,0 +1,10 @@
+import {Navigate} from "react-router-dom";
+import useToken from "../../hooks/useToken";
+
+export const ProtectedRoute = ({children}) => {
+    const {token} = useToken();
+    if (token === null) {
+        return <Navigate to="/login" replace/>;
+    }
+    return children;
+};
