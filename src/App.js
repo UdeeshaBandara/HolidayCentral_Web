18e09e92d311bdf55f1b3fbe3c32cc96521df937
udeeshabandara <udeeshabandara@gmail.com>
2023-05-03 22:31:07 +0530

    feat:applying protected route

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ import Home from "./screens/Home";
 import './fonts/NunitoSans-Bold.ttf'
 import {LocalizationProvider} from "@mui/x-date-pickers";
 import {AdapterMoment} from '@mui/x-date-pickers/AdapterMoment'
-import FlightSearch from "./components/FlightSearch";
+import FlightSearch from "./screens/FlightSearch";
 import Checkout from "./screens/Checkout";
+import {ProtectedRoute} from "./components/ProtectedRoute/index,jsx";
 import hotelcheckout from './screens/Hotel/HotelCheckout';
 import Hotelcheckouts from './screens/Hotel/HotelCheckout';
 import QuantityEdit from './screens/Hotel/Hotelcart';
@@ -18,23 +19,23 @@ import List from  './screens/Hotel/List';
 
 
 function App() {
-    return (
-        <LocalizationProvider dateAdapter={AdapterMoment}>
-            <Routes>
-                <Route exact path='/' element={<Home/>}/>
-                <Route path='/register' element={<Register/>}/>
-                <Route path='/login' element={<Login/>}/>
-                <Route exact path='/flight-search' element={<FlightSearch/>}/>
-                <Route exact path='/flight-checkout' element={<Checkout/>}/>
-                <Route exact path='/hotel' element={<HotelIn/>}/>
-                <Route exact path='/hotelcheckout' element={<Hotelcheckouts/>}/>
-                <Route exact path='/hotelcart' element={<QuantityEdit/>}/>
-                <Route exact path='/search' element={<SearchItem/>}/>
-                <Route exact path='/filter' element={<List/>}/>
-                
-            </Routes>
-        </LocalizationProvider>
-    );
+
+    return (<LocalizationProvider dateAdapter={AdapterMoment}>
+        <Routes>
+            <Route exact path='/' element={<ProtectedRoute><Home/></ProtectedRoute>}/>
+            <Route path='/register' element={<Register/>}/>
+            <Route path='/login' element={<Login/>}/>
+            <Route exact path='/flight-search'
+                   element={<ProtectedRoute><FlightSearch/></ProtectedRoute>}/>
+            <Route exact path='/flight-checkout'
+                   element={<ProtectedRoute><Checkout/></ProtectedRoute>}/>
+            <Route exact path='/hotel' element={<HotelIn/>}/>
+            <Route exact path='/hotelcheckout' element={<Hotelcheckouts/>}/>
+            <Route exact path='/hotelcart' element={<QuantityEdit/>}/>
+            <Route exact path='/search' element={<SearchItem/>}/>
+            <Route exact path='/filter' element={<List/>}/>
+        </Routes>
+    </LocalizationProvider>);
 }
 
 export default App;
